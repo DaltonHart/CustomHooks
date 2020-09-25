a1b788de638802cd04ec625d33b6e9954bf87c59
Dalton Hart <31603649+DaltonHart@users.noreply.github.com>
2020-09-24 17:59:11 -0700

    set up base of containers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+
+import Random from "./components/Random_V1";
+import Tag from "./components/Tag_V1";
+
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Random Gif Application</h1>
+      <h1>Random Pokemon Application</h1>
+      <div>
+        <Random />
+        <Tag />
+      </div>
     </div>
   );
 }
---
+++ src/components/Random_V1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Random() {
+  return (
+    <>
+      <h1>Random</h1>
+    </>
+  );
+}
+
+export default Random;
