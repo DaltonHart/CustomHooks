937110c5fae4b50861757d759c3a6f9a0e63ddea
Dalton Hart <31603649+DaltonHart@users.noreply.github.com>
2020-09-24 20:33:10 -0700

    adds fetch effect

--- src/components/Random_V1.jsx
+++ src/components/Random_V1.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Random() {
+  useEffect(function () {
+    const fetchPokemon = async function () {
+      const res = await fetch(
+        "https://totallyaccuratepokedex.herokuapp.com/api/v1/pokemon"
+      );
+      const { data } = await res.json();
+      console.log(data);
+    };
+    fetchPokemon();
+  }, []);
+
   return (
     <>
       <h1>Random</h1>
