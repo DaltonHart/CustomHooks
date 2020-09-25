4c10a99bda82d3c79f8b697428ccf069c93ba40e
Dalton Hart <31603649+DaltonHart@users.noreply.github.com>
2020-09-24 17:50:47 -0700

    Init Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Random Gif Application</h1>
     </div>
   );
 }
+
+export default App;
